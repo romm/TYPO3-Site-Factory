de74b6aeff51e40e5255966f0c4f7ad927159287
Romain Canon <romain.hydrocanon@gmail.com>
2015-07-20 17:26:55 +0000

    Dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,10 +3,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title'				=> 'Site Factory',
 	'state'				=> 'beta',
 	'version'			=> '0.1.1',
-	'description'		=> 'Replicate and modify an existing website model very easily with a flexible and lean design.' . LF .
-		'Read the code examples to understand and master all the TypoScript configuration, or extend the existing duplication processes.' . LF .
-		'Based on freesite (created by Kasper Skårhøj) this project was originaly conceived by Cyril Wolfangel and is developped and maintained by Romain Canon.' . LF .
-		'Join the project on https://github.com/romaincanon/TYPO3-Site-Factory',
+	'description'		=> 'Replicate and modify an existing website model very easily with a flexible and lean design. Read the code examples to understand and master all the TypoScript configuration, or extend the existing duplication processes. Based on freesite (created by Kasper Skårhøj) this project was originaly conceived by Cyril Wolfangel and is developped and maintained by Romain Canon. Join the project on https://github.com/romaincanon/TYPO3-Site-Factory',
 	'category'			=> 'module',
 
 	'constraints'		=> array(
