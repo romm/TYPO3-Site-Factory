bde2143ddf2e733ed586655df551c857ba4b4825
Romain Canon <romain.hydrocanon@gmail.com>
2016-04-05 21:36:28 +0200

    [TASK] Cleanup with PSR-4 standards

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,32 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-	'title'				=> 'Site Factory',
-	'state'				=> 'beta',
-	'version'			=> '0.1.1',
-	'description'		=> 'Replicate and modify an existing website model very easily with a flexible and lean design. Read the code examples to understand and master all the TypoScript configuration, or extend the existing duplication processes. Based on freesite (created by Kasper Skårhøj) this project was originaly conceived by Cyril Wolfangel and is developped and maintained by Romain Canon. Join the project on https://github.com/romaincanon/TYPO3-Site-Factory',
-	'category'			=> 'module',
+$EM_CONF[$_EXTKEY] = [
+    'title'       => 'Site Factory',
+    'state'       => 'beta',
+    'version'     => '0.1.1',
+    'description' => 'Replicate and modify an existing website model very easily with a flexible and lean design. Read the code examples to understand and master all the TypoScript configuration, or extend the existing duplication processes. Based on freesite (created by Kasper Skårhøj) this project was originaly conceived by Cyril Wolfangel and is developped and maintained by Romain Canon. Join the project on https://github.com/romaincanon/TYPO3-Site-Factory',
+    'category'    => 'module',
 
-	'constraints'		=> array(
-		'depends'			=> array(
-			'extbase'			=> '6.2',
-			'fluid'				=> '6.2',
-			'typo3'				=> '6.2.0-7.6.99'
-		),
-		'conflicts'			=> array(),
-		'suggests'			=> array()
-	),
+    'constraints' => [
+        'depends'   => [
+            'extbase' => '6.2',
+            'fluid'   => '6.2',
+            'typo3'   => '6.2.0-7.6.99'
+        ],
+        'conflicts' => [],
+        'suggests'  => []
+    ],
 
-	'author'			=> 'Romain CANON',
-	'author_email'		=> 'romain.canon@exl-group.com',
-	'author_company'	=> 'EXL Group',
+    'author'         => 'Romain CANON',
+    'author_email'   => 'romain.canon@exl-group.com',
+    'author_company' => 'EXL Group',
 
-	'shy'				=> '',
-	'priority'			=> '',
-	'module'			=> '',
-	'internal'			=> '',
-	'uploadfolder'		=> true,
-	'createDirs'		=> 'uploads/tx_sitefactory/_processed_/',
-	'modify_tables'		=> '',
-	'clearCacheOnLoad'	=> 1,
-	'lockType'			=> ''
-);
+    'shy'              => '',
+    'priority'         => '',
+    'module'           => '',
+    'internal'         => '',
+    'uploadfolder'     => true,
+    'createDirs'       => 'uploads/tx_sitefactory/_processed_/',
+    'modify_tables'    => '',
+    'clearCacheOnLoad' => 1,
+    'lockType'         => ''
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,39 +1,41 @@
 <?php
-if (!defined('TYPO3_MODE'))
-	throw new \Exception('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    throw new \Exception('Access denied.');
+}
 
 call_user_func(
-	function($extensionKey) {
-		if(TYPO3_MODE == 'BE') {
-			\Romm\SiteFactory\Utility\AjaxDispatcherUtility::activateAjaxDispatcher();
+    function ($extensionKey) {
+        if (TYPO3_MODE == 'BE') {
+            \Romm\SiteFactory\Utility\AjaxDispatcherUtility::activateAjaxDispatcher();
 
-			// Registering the caches for this extension.
-			$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_' . $extensionKey . '_main'] = array(
-				'backend'	=> 'TYPO3\\CMS\\Core\\Cache\\Backend\\SimpleFileBackend',
-				'frontend'	=> 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-				'groups'	=> array('all', 'system', 'pages')
-			);
+            // Registering the caches for this extension.
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_' . $extensionKey . '_main'] = [
+                'backend'  => 'TYPO3\\CMS\\Core\\Cache\\Backend\\SimpleFileBackend',
+                'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+                'groups'   => ['all', 'system', 'pages']
+            ];
 
-			// Registering the caches for this extension.
-			$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_' . $extensionKey . '_processed'] = array(
-				'backend'	=> 'TYPO3\\CMS\\Core\\Cache\\Backend\\SimpleFileBackend',
-				'frontend'	=> 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-				'options'	=> array(
-					'cacheDirectory'	=> Romm\SiteFactory\Core\Core::getProcessedFolderPath()
-				)
-			);
+            // Registering the caches for this extension.
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_' . $extensionKey . '_processed'] = [
+                'backend'  => 'TYPO3\\CMS\\Core\\Cache\\Backend\\SimpleFileBackend',
+                'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+                'options'  => [
+                    'cacheDirectory' => Romm\SiteFactory\Core\Core::getProcessedFolderPath()
+                ]
+            ];
 
-			// Including main TypoScript files.
-			$includeTypoScriptSyntax = '<INCLUDE_TYPOSCRIPT: source="FILE:%s">';
-			$typoScriptFiles = array(
-				'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/DefaultConfiguration.ts',
-				'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/FieldsTypesConfiguration.ts',
-				'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/FieldsConfiguration.ts',
-				'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/DuplicationConfiguration.ts'
-			);
-			foreach ($typoScriptFiles as $filePath)
-				\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(sprintf($includeTypoScriptSyntax, $filePath));
-		}
-	},
-	$_EXTKEY
+            // Including main TypoScript files.
+            $includeTypoScriptSyntax = '<INCLUDE_TYPOSCRIPT: source="FILE:%s">';
+            $typoScriptFiles = [
+                'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/DefaultConfiguration.ts',
+                'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/FieldsTypesConfiguration.ts',
+                'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/FieldsConfiguration.ts',
+                'EXT:' . $extensionKey . '/Configuration/TypoScript/Default/DuplicationConfiguration.ts'
+            ];
+            foreach ($typoScriptFiles as $filePath) {
+                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(sprintf($includeTypoScriptSyntax, $filePath));
+            }
+        }
+    },
+    $_EXTKEY
 );
